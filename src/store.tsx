2ef1c2fb3a5ab999560cf0660baeb66da81d059b
Mofizul <mofizul21@gmail.com>
2023-01-30 19:07:36 +0600

    Update package

--- src/store.tsx
+++ src/store.tsx
@@ -1,4 +1,4 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/auth/AuthSlice';
 import movieReducer from './features/movies/movieSlice';
 
