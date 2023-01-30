a505df3eb7541de72d36ee01da953d1ef5367f5f
Mofizul <mofizul21@gmail.com>
2023-01-30 18:49:50 +0600

    first commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+	Navigate,
+} from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
+import { theme } from './shared/utils/validation/themes';
+import RegisterPage from './pages/Register.page';
+import SigninPage from './pages/Signin.page';
+import HomePage from './pages/Home.page';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ThemeProvider theme={theme}>
+			<Router>
+				<Routes>
+					<Route path="/" element={<HomePage />}></Route>
+					<Route path="/register" element={<RegisterPage />}></Route>
+					<Route path="/signin" element={<SigninPage />}></Route>
+					<Route path="*" element={<Navigate to="/signin" />} />
+				</Routes>
+			</Router>
+		</ThemeProvider>
+	);
 }
 
 export default App;
